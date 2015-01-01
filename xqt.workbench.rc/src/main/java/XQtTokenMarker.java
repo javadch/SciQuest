d7603a1d5781551c5d86e89b56f6c259c816dba6
javadch <chamanara@gmail.com>
2015-01-01 22:22:28 +0100

    added basic string and math functions

--- xqt.workbench.rc/src/main/java/XQtTokenMarker.java
+++ xqt.workbench.rc/src/main/java/XQtTokenMarker.java
@@ -96,44 +96,67 @@ public class XQtTokenMarker extends SQLTokenMarker {
         xqtKeywords.add("date",Token.KEYWORD2);            
     }
 
+    // this method should grab all the information from the functions registered in the defalt adapter.
     private static void addSystemFunctions(){
-        xqtKeywords.add("ABS",Token.KEYWORD2);
+        // MATH functions
+        xqtKeywords.add("ABS",Token.KEYWORD2); //Returns the absolute value of a number.
         xqtKeywords.add("ACOS",Token.KEYWORD2);
         xqtKeywords.add("ASIN",Token.KEYWORD2);
         xqtKeywords.add("ATAN",Token.KEYWORD2);
         xqtKeywords.add("ATN2",Token.KEYWORD2);
-        xqtKeywords.add("CEILING",Token.KEYWORD2);
-        xqtKeywords.add("CHARINDEX",Token.KEYWORD2);
+        xqtKeywords.add("CEILING",Token.KEYWORD2); // Rounds a noninteger value upwards to the next greatest integer. Returns an integer value unchanged.
         xqtKeywords.add("COS",Token.KEYWORD2);
-        xqtKeywords.add("COT",Token.KEYWORD2);
-        xqtKeywords.add("CURRENT_TIME",Token.KEYWORD2);
-        xqtKeywords.add("CURRENT_DATE",Token.KEYWORD2);
-        xqtKeywords.add("CURRENT_TIMESTAMP",Token.KEYWORD2);
-        xqtKeywords.add("DAY",Token.KEYWORD2);
-        xqtKeywords.add("FLOOR",Token.KEYWORD2);
-        xqtKeywords.add("ISDATE",Token.KEYWORD2);
-        xqtKeywords.add("ISNULL",Token.KEYWORD2);
-        xqtKeywords.add("ISNUMERIC",Token.KEYWORD2);
-        xqtKeywords.add("LOG",Token.KEYWORD2);
-        xqtKeywords.add("LOG10",Token.KEYWORD2);
-        xqtKeywords.add("LOWER",Token.KEYWORD2);
-        xqtKeywords.add("LTRIM",Token.KEYWORD2);
-        xqtKeywords.add("MONTH",Token.KEYWORD2);
-        xqtKeywords.add("PI",Token.KEYWORD2);
-        xqtKeywords.add("POWER",Token.KEYWORD2);
+        xqtKeywords.add("EXP",Token.KEYWORD2); // Raises a value to the power of the mathematical constant known as e.
+        xqtKeywords.add("FLOOR",Token.KEYWORD2); // Rounds a noninteger value downwards to the next least integer. Returns an integer value unchanged.
+        xqtKeywords.add("LOG",Token.KEYWORD2); // Returns the natural logarithm of a number.
+        xqtKeywords.add("LOG10",Token.KEYWORD2); // Returns the base 10 logarithm of a number.
+        xqtKeywords.add("LOGN",Token.KEYWORD2); // Returns the base N logarithm of a number.
+        //xqtKeywords.add("PI",Token.KEYWORD2); // to be defined in the grammar
+        xqtKeywords.add("POWER",Token.KEYWORD2); // Raises a number to a specified power.
         xqtKeywords.add("RAND",Token.KEYWORD2);
-        xqtKeywords.add("REPLACE",Token.KEYWORD2);
-        xqtKeywords.add("RIGHT",Token.KEYWORD2);
         xqtKeywords.add("ROUND",Token.KEYWORD2);
-        xqtKeywords.add("RTRIM",Token.KEYWORD2);
         xqtKeywords.add("SIN",Token.KEYWORD2);
-        xqtKeywords.add("SQRT",Token.KEYWORD2);
-        xqtKeywords.add("SQUARE",Token.KEYWORD2);
-        xqtKeywords.add("STR",Token.KEYWORD2);
-        xqtKeywords.add("SUBSTRING",Token.KEYWORD2);
+        xqtKeywords.add("SQRT",Token.KEYWORD2); // Computes the square root of a number.
         xqtKeywords.add("TAN",Token.KEYWORD2);
-        xqtKeywords.add("UPPER",Token.KEYWORD2);
-        xqtKeywords.add("YEAR",Token.KEYWORD2);
+        xqtKeywords.add("DEGREEOF",Token.KEYWORD2); // Converts an angle measured in radians to an approximately equivalent angle measured in degrees
+        xqtKeywords.add("RADIANSOF",Token.KEYWORD2); // Converts an angle measured in degrees to an approximately equivalent angle measured in radians
+        
+        // STRING functions
+        xqtKeywords.add("INDEXOF",Token.KEYWORD2); // Returns an integer value representing the starting position of a string within the search string.
+        xqtKeywords.add("ISDATE",Token.KEYWORD2);
+        xqtKeywords.add("ISEMPTY",Token.KEYWORD2);
+        xqtKeywords.add("ISNUMERIC",Token.KEYWORD2);
+        xqtKeywords.add("LASTINDEXOF",Token.KEYWORD2); // Returns an integer value representing the last starting position of a string within the search string.
+        xqtKeywords.add("LENGTH",Token.KEYWORD2); // Returns an integer value representing the number of characters in a string expression.
+        xqtKeywords.add("LOWER",Token.KEYWORD2); // Converts a string to all lowercase characters.
+        xqtKeywords.add("LTRIM",Token.KEYWORD2); // Removes leading characters from a character string.
+        xqtKeywords.add("REPLACE",Token.KEYWORD2); // Replaces all occurrences of a specified string value with another string value.
+        xqtKeywords.add("REVERSE",Token.KEYWORD2); // Returns the reverse of a string value.
+        xqtKeywords.add("RTRIM",Token.KEYWORD2); // Removes trailing characters from a character string.
+        xqtKeywords.add("SUBSTRING",Token.KEYWORD2); // Returns a portion of a string.
+        xqtKeywords.add("TRIM",Token.KEYWORD2); // Removes leading and trailing characters from a character string.
+        xqtKeywords.add("UPPER",Token.KEYWORD2); // Converts a string to all uppercase characters
+                
+        // DATE/ TIME
+        xqtKeywords.add("DATE",Token.KEYWORD2); // current date
+        xqtKeywords.add("DATEOF",Token.KEYWORD2); // date object of the passed string
+        xqtKeywords.add("DAY",Token.KEYWORD2); // 
+        xqtKeywords.add("HOUR",Token.KEYWORD2); // hour component of a time object
+        xqtKeywords.add("ISLEAPYEAR",Token.KEYWORD2);
+        xqtKeywords.add("MINUTE",Token.KEYWORD2);
+        xqtKeywords.add("MONTH",Token.KEYWORD2);
+        xqtKeywords.add("SECOND",Token.KEYWORD2);
+        xqtKeywords.add("TIME",Token.KEYWORD2); //current time in the system timezone
+        xqtKeywords.add("TIMEOF",Token.KEYWORD2); //time object of the passed string
+        xqtKeywords.add("TIMESTAMP",Token.KEYWORD2); // current date and time
+        xqtKeywords.add("TIMESTAMP",Token.KEYWORD2); // datetime object of the passed string
+        xqtKeywords.add("WEEK",Token.KEYWORD2); 
+        xqtKeywords.add("WEEKDAY",Token.KEYWORD2); //Returns the week day name/id of the passed date object.
+        xqtKeywords.add("YEAR",Token.KEYWORD2); // year component of a date object
+
+        
+        xqtKeywords.add("ISNULL",Token.KEYWORD2);
+        //xqtKeywords.add("RIGHT",Token.KEYWORD2);
     }
 
     private static void addOperators(){           
